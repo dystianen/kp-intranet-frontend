a09e3776812cbc2371763b5ef7ce02d3e71acff6
agik-setiawan <setiawanagik@gmail.com>
2023-05-05 16:23:10 +0700

    reset ujian

--- src/app/modules/admin/ujian/reset-ujian/reset-ujian.component.ts
+++ src/app/modules/admin/ujian/reset-ujian/reset-ujian.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { identity, pickBy } from 'lodash';
+import { ResetUjianService } from './reset-ujian.service';
 
 @Component({
-  selector: 'app-reset-ujian',
-  templateUrl: './reset-ujian.component.html',
-  styleUrls: ['./reset-ujian.component.scss']
+    selector: 'app-reset-ujian',
+    templateUrl: './reset-ujian.component.html',
+    styleUrls: ['./reset-ujian.component.scss'],
 })
 export class ResetUjianComponent implements OnInit {
+    form: FormGroup;
+    isLoading: boolean = false;
 
-  constructor() { }
+    ujians: [] = [];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private formBuilder: FormBuilder,
+        private resetUjianService: ResetUjianService
+    ) {}
 
+    ngOnInit(): void {
+        this.form = this.formBuilder.group({
+            email: '',
+            start_date: '',
+            end_date: '',
+        });
+    }
+
+    findData(f: NgForm) {
+        if (!f.valid) {
+            return;
+        }
+        this.isLoading = true;
+        const data = pickBy(f.value, identity);
+        this.resetUjianService.findUjian(data).subscribe((res) => {
+            console.log('abc', res);
+            this.ujians = res;
+            this.isLoading = false;
+        });
+    }
+
+    resetData(student_id: number, assign_auto_id: number) {
+        this.isLoading = true;
+        this.resetUjianService
+            .resetUjian({ student_id, assign_auto_id })
+            .subscribe((res) => {
+                const form: any = this.form;
+                this.findData(form);
+                this.ujians = res;
+                this.isLoading = false;
+            });
+    }
 }
--- src/app/modules/admin/ujian/reset-ujian/reset-ujian.module.ts
+++ src/app/modules/admin/ujian/reset-ujian/reset-ujian.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ResetUjianComponent } from './reset-ujian.component';
 import { RouterModule, Routes } from '@angular/router';
 import { CrudModule } from 'app/shared/crud/crud.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
 
 const routes: Routes = [
     {
@@ -13,6 +15,14 @@ const routes: Routes = [
 
 @NgModule({
     declarations: [ResetUjianComponent],
-    imports: [CommonModule,CrudModule,RouterModule.forChild(routes)],
+    imports: [
+        CommonModule,
+        CrudModule,
+        RouterModule.forChild(routes),
+        MatDatepickerModule,
+        NgxMatNativeDateModule,
+        NgxMatDatetimePickerModule,
+        NgxMatTimepickerModule,
+    ],
 })
 export class ResetUjianModule {}
